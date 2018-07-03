a476e3025555386ab4ecb17eb5db1145bf13598a
Cory Lashway <cory.lashway@gmail.comc>
2018-07-03 17:42:24 -0400

    Add description

--- ecr/nuxt.config.js
+++ ecr/nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'EC Regalia Personal Website' }
     ],
     script: [
       { src: 'https://www.gstatic.com/firebasejs/5.2.0/firebase.js'},
