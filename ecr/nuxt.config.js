043f88c7c71fa9b6d8eab6acc00d4ccfb87217fa
Cory Lashway <cory.lashway@gmail.comc>
2018-07-03 17:41:10 -0400

    Add firebase lib

--- ecr/nuxt.config.js
+++ ecr/nuxt.config.js
@@ -9,6 +9,21 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
+    script: [
+      { src: 'https://www.gstatic.com/firebasejs/5.2.0/firebase.js'},
+      { innerHTML: `// Initialize Firebase
+        var config = {
+          apiKey: "AIzaSyAUT6bxa7dsOraG4Qb6N1RZ47n30B-f1GI",
+          authDomain: "ecregalia6.firebaseapp.com",
+          databaseURL: "https://ecregalia6.firebaseio.com",
+          projectId: "ecregalia6",
+          storageBucket: "ecregalia6.appspot.com",
+          messagingSenderId: "427320962979"
+        };
+        firebase.initializeApp(config);
+      `,
+      type: 'text/javascript', charset: 'utf-8'}
+    ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
